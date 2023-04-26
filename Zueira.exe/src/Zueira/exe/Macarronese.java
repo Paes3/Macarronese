cc2bdc6c5f2e8820848632d1b41f99a34cded8fe
seapi <seapi@DESKTOP-KB1M05V>
2023-04-25 23:14:12 -0300

    commit initial

---
+++ Zueira.exe/src/Zueira/exe/Macarronese.java
@@ -0,0 +1,15 @@
+package Zueira.exe;
+
+public class Macarronese {
+
+	public static void main(String[] args) {
+		
+		int i = 3;
+        while (i == 3) {
+        	
+            System.out.println("macarronese");
+        }
+
+	}
+
+}
