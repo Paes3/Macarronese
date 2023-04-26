3d2b39f23901fbc3001cb444c63816174cd8e9f2
Paes3 <131565966+Paes3@users.noreply.github.com>
2023-04-25 23:25:10 -0300

    Update Macarronese.java

--- Zueira.exe/src/Zueira/exe/Macarronese.java
+++ Zueira.exe/src/Zueira/exe/Macarronese.java
@@ -5,11 +5,9 @@ public class Macarronese {
 	public static void main(String[] args) {
 		
 		int i = 3;
-        while (i == 3) {
+ 		while (i == 3) {
         	
             System.out.println("macarronese");
         }
-
 	}
-
 }
